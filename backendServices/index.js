816c0a93e92477a7ee4909a9d649655e05aff491
Gauri <ggauri97@gmail.com>
2023-05-09 21:33:42 -0700

    added the sql views for categories + products and added make valid user apis

--- backendServices/index.js
+++ backendServices/index.js
@@ -72,9 +72,9 @@ app.post('/login', (req, res) => {
 });
 
 app.post('/register', (req, res) => {
-    const { firstname, lastname, phone_number, loc_id } = req.body;
+    const { firstname, lastname, phone_number, loc_id, user_id } = req.body;
     console.log(firstname, lastname, phone_number)
-    pool.query('INSERT INTO `users` (`firstname`, `lastname`, `phone_number`, `loc_id`) VALUES (?, ?, ?, ?)', [firstname, lastname, phone_number, loc_id], function (err, result) {
+    pool.query('INSERT INTO `users` (`firstname`, `lastname`, `phone_number`, `loc_id`,`id`,`valid_user`) VALUES (?, ?, ?, ?, ?,0)', [firstname, lastname, phone_number, loc_id, user_id], function (err, result) {
 
         if (err) {
             if (err.code === 'ER_DUP_ENTRY') {
@@ -93,21 +93,70 @@ app.post('/register', (req, res) => {
         }
     });
 });
+app.post('/makeValid', (req, res) => {
+    const { user_id } = req.body;
+    pool.query('Update `users` SET valid_user = 1 WHERE id = ? ', [user_id], function (err, result) {
+        if (err) {
+            if (err.code === 'ER_EMPTY_QUERY') {
+                res.status(409).send('User does not exist');
+            } else {
+                console.log('An error occured.')
+                res.status(500).send(err.toString());
+            }
+        } else {
+            console.log(result);
+            console.log(result.affectedRows);
+            if (result.length === 0 || result.affectedRows === 0) {
+                res.status(403).send('User id is invalid');
+            }
+            else if (result.changedRows)
+                res.status(201).send('User is Validated');
+            else
+                res.status(201).send('No changes were made');
+        }
+    });
+});
+app.post('/isRegistered', (req, res) => {
+    const { user_id } = req.body;
+    pool.query('SELECT valid_user FROM users WHERE id = ?', [user_id], function (err, result) {
+        if (err) {
+            console.log('An error occured.')
+            res.status(500).send(err.toString());
+        } else {
+            console.log(result);
+            console.log(result.changedRows);
+            if (result.length === 0 || result[0].valid_user === null) {
+                res.status(403).send('User id is not invalid');
+            }
+            else if (result[0].valid_user == 1) {
+                res.status(201).send('User is Registered');
+            }
+            else {
+                res.status(201).send('User is not Registered');
+            }
+        }
+    });
+});
 //Get all product Categories
 app.get('/categories', (req, res) => {
-    pool.query('SELECT DISTINCT category FROM products', (err, result) => {
+    const { postal_code } = req.query;
+    pool.query('SELECT DISTINCT category FROM vw_product_geo_avail WHERE postal_code = ? AND stock_avail > ?', [postal_code, 0], (err, result) => {
         if (err) {
             console.log('An error occurred.');
             res.status(500).send(err.toString());
         } else {
-            res.status(201).send(JSON.stringify(result.rows));
+            const categories = result.map(obj => obj.category);
+            console.log(categories);
+            res.status(201);
+            res.send(JSON.stringify(categories));
         }
     });
 });
 //Get all products
 app.get('/products', (req, res) => {
-    const { category } = req.query;
-    pool.query('SELECT * FROM products WHERE category = ?', [category], (err, result) => {
+    const { category, postal_code } = req.query;
+
+    pool.query('SELECT * FROM vw_product_geo_avail WHERE postal_code = ? AND category = ? AND stock_avail > ? ', [postal_code, category, 0], (err, result) => {
         if (err) {
             if (err.code === 'ENOENT') {
                 res.status(409).send('Category is not present');
@@ -116,14 +165,26 @@ app.get('/products', (req, res) => {
                 res.status(500).send(err.toString());
             }
         } else {
-            res.status(201).send(JSON.stringify(result.rows));
+            console.log(result);
+            const products = result.map(row => {
+                return {
+                    id: row.id,
+                    name: row.name,
+                    category: row.category,
+                    price: row.price,
+                    description: row.description,
+                    img_url: row.img_url,
+                    created_datetime: row.created_datetime
+                };
+            });
+            res.status(200).json(products);
         }
     });
 });
 // Get product desription
 app.get('/productDescription', (req, res) => {
     const { id } = req.query;
-    pool.query('SELECT description FROM products WHERE id = ?', [id], (err, result) => {
+    pool.query('SELECT * FROM products WHERE id = ?', [id], (err, result) => {
         if (err) {
             if (err.code === 'ENOENT') {
                 res.status(409).send('Product is not present');
@@ -132,8 +193,19 @@ app.get('/productDescription', (req, res) => {
                 res.status(500).send(err.toString());
             }
         } else {
+            const product = result.map(row => {
+                return {
+                    id: row.id,
+                    name: row.name,
+                    category: row.category,
+                    price: row.price,
+                    description: row.description,
+                    img_url: row.img_url,
+                    created_datetime: row.created_datetime
+                };
+            });
+            res.status(200).json(product);
             // have to check if product id not present error would come or should i just add a check here for size of rows?
-            res.status(201).send(JSON.stringify(result.rows));
         }
     });
 });
@@ -147,8 +219,7 @@ app.get('/subscriptions/getSubscriptions', (req, res) => {
         if (err) {
             if (err.code === 'ENOENT') {
                 res.status(409).send('No subscriptions found!');
-            }else
-            {
+            } else {
                 console.log('An error occured.')
                 res.status(500).send(err.toString());
             }
@@ -161,7 +232,7 @@ app.get('/subscriptions/getSubscriptions', (req, res) => {
 
 app.post('/subscriptions/subscribe', (req, res) => {
     const { user_id, slot, sub_start_date, sub_end_date, freq, quantity, item_id } = req.body;
-    pool.query('INSERT INTO `subscriptions` (`user_id`, `item_id`, `sub_start_date`, `sub_end_date`,`freq`,`quantity`,`slot`,`is_active` ) VALUES (?, ?, ?, ?,?, ?, ?, 1) ON DUPLICATE KEY UPDATE sub_end_date= VALUES(sub_end_date),quantity = VALUES(quantity),freq = VALUES(freq) ', [user_id, item_id, sub_start_date, sub_end_date,freq,quantity,slot], function (err, result) {
+    pool.query('INSERT INTO `subscriptions` (`user_id`, `item_id`, `sub_start_date`, `sub_end_date`,`freq`,`quantity`,`slot`,`is_active` ) VALUES (?, ?, ?, ?,?, ?, ?, 1) ON DUPLICATE KEY UPDATE sub_end_date= VALUES(sub_end_date),quantity = VALUES(quantity),freq = VALUES(freq) ', [user_id, item_id, sub_start_date, sub_end_date, freq, quantity, slot], function (err, result) {
 
         if (err) {
             if (err.code === 'ER_DUP_ENTRY') {
